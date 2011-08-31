70d21b8073482a59849111dc289efe32c8a41e88
Christopher Brown <io@henrian.com>
2011-08-31 12:42:17 -0500

    All my nice node.js wrappers, trying to DRY up my node.js one-off projects. (Initial commit.)

---
+++ index.js
@@ -0,0 +1,12 @@
+// additive
+exports.http = require("./lib/http");
+exports.mongo = require("./lib/mongo");
+exports.sql = require("./lib/sql");
+
+// destructive
+exports.array = require("./lib/array");
+exports.date = require("./lib/date");
+exports.json = require("./lib/json");
+exports.numbers = require("./lib/numbers");
+exports.string = require("./lib/string");
+
