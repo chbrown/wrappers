1a47a235c8339fb8940f6eca39b13000578884ea
Christopher Brown <io@henrian.com>
2011-10-12 12:26:08 -0500

    Added little countdown/semaphore snippet.

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ exports.json = require("./lib/json");
 exports.numbers = require("./lib/numbers");
 exports.string = require("./lib/string");
 
+exports.countdown = function(total, callback) {
+  var callback_args = Array.prototype.slice.call(arguments, 2);
+  return function() {
+    if (!--total) {
+      callback.apply(null, callback_args);
+    }
+  };
+};
+
